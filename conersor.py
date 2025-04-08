ada1a46d3f9ad4b884808d37d85224497f255b3e
Nicolas Daniel de Souza Bastos <nicolasdaniel16y@gmail.com>
2025-04-07 22:22:16 -0300

    Add files via upload

---
+++ conersor.py
@@ -0,0 +1,7 @@
+def converter_fonte(texto):
+    return texto.swapcase()
+
+# Exemplo de uso
+entrada = input("Digite um texto: ")
+resultado = converter_fonte(entrada)
+print("Texto convertido:", resultado)
